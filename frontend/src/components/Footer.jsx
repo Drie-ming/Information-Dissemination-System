95b974be6bad97b9d8b06b3da471341e7f70b6e9
Drie-ming <alexandrie2k69@gmail.com>
2025-10-22 00:02:33 +0800

    may registration na(ui only) tas  tig ayos ki diit su paka design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Button } from './ui/button';
 
 const Footer = () => {
+
+const yearDate = new Date().getFullYear();
+
     return (
         <>
          <div className='flex items-center justify-center py-16 bg-amber-300 '>
-            <p className='font-bold text-3xl '>Footer</p>
+            <div className='font-bold text-3xl '>&copy; {yearDate} Barangay Lidong. All rights reserved</div>
            
         </div>
         </>
--- frontend/src/components/ResHomePage.jsx
+++ frontend/src/components/ResHomePage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import IdLogo from "../assets/IdLogo.png";
 import { Card, CardContent } from "@/components/ui/card";
+import { events } from "./exEvents";
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 import {
   Carousel,
   CarouselContent,
@@ -9,98 +12,125 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-const ResHomePage = () => {
-  const events = [
-    {
-      what: "Tech Startup Pitch Night",
-      when: "November 5, 2025 at 6:00 PM",
-      where: "Launchpad Manila, Makati City",
-    },
-    {
-      what: "Barangay Blood Donation Drive",
-      when: "October 28, 2025 from 9:00 AM to 3:00 PM",
-      where: "Barangay Hall, Quezon City",
-    },
-    {
-      what: "React Router v7 Workshop",
-      when: "July 12, 2025 at 2:00 PM",
-      where: "Online via Zoom",
-    },
-    {
-      what: "Akap",
-      when: "September 12, 2025 at 2:00 PM",
-      where: "Barangay hall of lidong",
-    },
-    {
-      what: "Libre Tuli",
-      when: "May 12, 2025 at 2:00 PM",
-      where: "Barangay hall of lidong",
-    },
-  ];
-  const names = ["Alice", "Bob", "Charlie", "Diana", "Ethan"];
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
+import { Button } from "./ui/button";
+
+const ResHomePage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-amber-100 ">
-        <div className="flex flex-row items-center justify-center mt-10 ">
+      <div className="flex flex-col items-center justify-center min-h-dvh bg-green-200 relative p-10">
+        <div className="flex flex-wrap items-center justify-center mt-10 relative">
           <div className="ml-5">
             <img className="h-150 w-150 " src={IdLogo} alt="I.D Logo" />
           </div>
 
           <div className="flex  items-center justify-center ">
-            <div className="ml-2 mr-5  text-center text-4xl ">
-              <p className=" w-2xl text-center">
-                Your trusted source for accurate and verified public updates.
-                Stay informed, Stay safe, Stay connected.
-              </p>
+            <div className="ml-2 mr-5  text-center text-3xl ">
+              <div className=" w-2xl text-left">
+                <strong className="text-5xl">
+                  Built for clarity. Powered by trust. Designed for every
+                  resident.
+                </strong>
+                <br /> <br /> Barangay Lidong's authenticated announcements
+                straight to residents. Featuring Real-time SMS alerts keep you
+                informed of emergency notices, community events, and public
+                service reminders directly on your mobile phone. <br /> <br />
+                <Dialog>
+                  <form>
+                    <DialogTrigger asChild>
+                      <Button variant="outline" className=' text-white text-2xl h-15 bg-blue-400 hover:bg-blue-700 hover:text-white '>Register your phone number</Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px] bg-green-100" >
+                      <DialogHeader>
+                        <DialogTitle>Phone Registration</DialogTitle>
+                        <DialogDescription>
+                          Click register when
+                          you&apos;re done.
+                        </DialogDescription>
+                      </DialogHeader>
+                      <div className="grid gap-4">
+                        <div className="grid gap-3">
+                          <Label htmlFor="name-1">Purok</Label>
+                          <Input
+                            id="name-1"
+                            name="name"
+                            defaultValue="Ex: 4"
+                          />
+                        </div>
+                        <div className="grid gap-3">
+                          <Label htmlFor="phoneNumber-1">Phone number</Label>
+                          <Input
+                            id="phoneNumber-1"
+                            name="phoneNumber"
+                            defaultValue="Ex: 09XXXXXXXXX"
+                          />
+                        </div>
+                      </div>
+                      <DialogFooter>
+                        <DialogClose asChild>
+                          <Button variant="outline" className='hover:bg-red-500'>Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit" className='hover:bg-green-500'>Register</Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </form>
+                </Dialog>
+              </div>
             </div>
           </div>
-
-
-
-          
         </div>
       </div>
 
-      <div className="border-3 flex items-center justify-center min-h-screen   bg-amber-200">
-        
-          <Carousel
-            opts={{ align: "start" }}
-            orientation="horizontal"
-            className="w-full max-w-5xl"
-          >
-            <CarouselContent className="-ml-1 h-[350px]">
-              {events.map((event, index) => (
-                <CarouselItem
-                  key={index}
-                  className="pl-1 basis-full md:basis-1/2 lg:basis-1/1"
-                >
-                  <div className="p-4">
-                    <Card className="h-[300px] w-full shadow-lg">
-                      <CardContent className="flex flex-col justify-center items-center h-full p-10 space-y-6">
-                        <span className="text-3xl font-bold">{event.what}</span>
-                        <span className="text-lg text-muted-foreground">
-                          {event.when}
-                        </span>
-                        <span className="text-lg text-muted-foreground">
-                          {event.where}
-                        </span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-       
+      <div className="border-3 flex flex-col items-center justify-center min-h-screen   bg-amber-200">
+        <div className="mb-20 font-bold text-3xl">
+          <h1>Events</h1>
+        </div>
+
+        <Carousel
+          opts={{ align: "start" }}
+          orientation="horizontal"
+          className="w-full max-w-5xl"
+        >
+          <CarouselContent className="-ml-1 h-[350px]">
+            {events.map((event, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 basis-full md:basis-1/2 lg:basis-1/1 "
+              >
+                <div className="p-4">
+                  <Card className="h-[300px] w-full shadow-lg bg-green-200">
+                    <CardContent className="flex flex-col justify-center items-center h-full p-10 space-y-6">
+                      <span className="text-3xl font-bold">{event.what}</span>
+                      <span className="text-lg text-muted-foreground">
+                        {event.when}
+                      </span>
+                      <span className="text-lg text-muted-foreground">
+                        {event.where}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
-    
+
       <div className="border-2 h-screen">weather app</div>
     </>
     //makasta kang weather app tas registration pag mata
-    
   );
 };
 
